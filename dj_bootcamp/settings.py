cb8c26c6e1d4a446ab89a3e8288ff583ca78c5ce
root <ririro93@gmail.com>
2020-10-10 14:00:43 +0000

    secret key removed

--- dj_bootcamp/settings.py
+++ dj_bootcamp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from secret_key import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=v&x%k71ctjmbl@tiw+-sf^np105fwx+zm1^*9flt@rc77@oou'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
