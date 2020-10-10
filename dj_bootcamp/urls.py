841fd8ee94eebf8fe46f67cc65299c5d2f22b892
root <ririro93@gmail.com>
2020-10-10 14:47:04 +0000

    new views

--- dj_bootcamp/urls.py
+++ dj_bootcamp/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from products import views
+from products.views import (
+    home_view,
+    product_detail_view,
+    product_api_detail_view,
+)
 
 urlpatterns = [
-    path('search/', views.home_view),
+    path('search/', home_view),
+    path('products/<int:pk>/', product_detail_view),
+    path('products-api/<int:pk>/', product_api_detail_view),
     path('admin/', admin.site.urls),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,24 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse, Http404
 from django.shortcuts import render
+
+from .models import Product
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Hello World </h1>")
+
+def product_detail_view(request, pk):
+    try:
+        obj = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        raise Http404
+        
+    return HttpResponse(f"Product title is {obj.title}")
+
+def product_api_detail_view(request, pk):
+    try:
+        obj = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return JsonResponse({"message": "Not found"})
+    return JsonResponse({"id": obj.pk})
+    
